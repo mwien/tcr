d83fd1083ae4d7ae0896657958ca80310e0e2d08
Marcel Wienöbst <marcel.wienoebst@student.uni-luebeck.de>
2018-06-12 11:54:53 +0200

    bla

---
+++ template.cc
@@ -0,0 +1,14 @@
+/* NAME
+** n^?
+* DESCRIPTION \\
+* \emph{MORE} DESCRIPTION
+*/
+
+int main() {
+    // note that the code can still be compiled and tested
+    // and only the code between START and END will appear on the document
+}
+
+//START
+// < insert algorithm >
+//END
